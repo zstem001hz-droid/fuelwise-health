7b7ab489b6e8e55954164d5ee91806c1659a0808
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-05 14:36:04 -0400

    fix: add vite-env.d.ts to resolve CSS import TypeScript error | wired App and Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
