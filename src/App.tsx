7a90515884de7eb65a18b5141b11f85b9b3a3bd8
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-05 15:15:57 -0400

    feat(router): implement basic routing with Home and Dashboard pages, style Navbar with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+
+function HomePage() {
+  return <div>Home Page</div>;
+}
+
+function DashboardPage() {
+  return <div>Dashboard Page</div>;
+}
 
 export default function App() {
-	return (
-		<Navbar />
-	);
+  return (
+    <>
+      <main>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,18 @@
-
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="w-full h-16 px-6 flex items-center justify-between">
+      <h1 className="text-lg font-semibold">FuelWise</h1>
 
-      <div>
-        <h1>FuelWise</h1>
-      </div>
-
-      <div>
-        <ul>
-          <li>Home</li> 
-          <li>Dashboard</li>
-        </ul>
-      </div>
-
+      <ul className="flex items-center gap-6 text-sm">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
