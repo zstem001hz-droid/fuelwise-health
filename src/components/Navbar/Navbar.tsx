17452e8f2c493f3f79ee07e30c362823d93536a2
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-05 14:08:48 -0400

    navbar: replace empty component with nav markup and brand header

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 
 
 export default function Navbar() {
+  return (
+    <nav className="navbar">
 
-    return (
-        
-    )
+      <div>
+        <h1>FuelWise</h1>
+      </div>
+
+      <div>
+        <ul>
+          <li>Home</li> 
+          <li>Dashboard</li>
+        </ul>
+      </div>
+
+    </nav>
+  );
 }
