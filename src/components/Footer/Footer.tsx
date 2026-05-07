f6e46d8b535c820a64cb04ff8076e07a994901a4
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-07 15:05:48 -0400

    (footer): import link from react router for footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+
+
+
 export default function Footer() {
   return (
     <footer className="border-t border-white/10 bg-[#111315]/85 backdrop-blur-md text-center text-sm text-stone-300 py-6 mt-12">
