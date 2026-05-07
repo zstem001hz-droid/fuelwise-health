6fb6879827c99c7c0e94c227865c1e5907649ac8
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-07 12:43:56 -0400

    change z index on navigation bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="absolute top-0 left-0 z-20 w-full">
+    <nav className="absolute top-0 left-0 z-40 w-full">
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
       <h1 className="text-lg text-white font-semibold">FuelWise</h1>
 
